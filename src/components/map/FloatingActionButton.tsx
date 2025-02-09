4848c10d96c6dfc5d005d510624f9653981756ec
muffin9 <jinha6707@gmail.com>
2025-02-10 00:13:43 +0900

    refactor: BottomDrawer 네이밍 오타 수ㅇ

--- src/components/map/FloatingActionButton.tsx
+++ src/components/map/FloatingActionButton.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 import { Button } from '@/components/ui/button';
-import { BottomDrawer } from '@/components/common/BototmDrawer';
+import { BottomDrawer } from '@/components/common/BottomDrawer';
 import { useRouter } from 'next/navigation';
 
 const FloatingActionButton = () => {
