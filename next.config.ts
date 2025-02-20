c4cc74f889d58e07f9492c014198c8777da380eb
muffin9 <jinha6707@gmail.com>
2025-02-20 10:04:32 +0900

    chore: vercel http api test

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*', // 클라이언트가 호출할 경로
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
