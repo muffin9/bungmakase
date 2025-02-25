6ac22f2c6874dd12ab7a7c4c9f6ae8f56ac33a34
muffin9 <jinha6707@gmail.com>
2025-02-25 10:21:05 +0900

    refactor: 쿠키에 담아오는 이름 token으로 변경

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,7 @@ import type { NextRequest } from 'next/server';
 import { shouldRedirectToHome, shouldRedirectToLogin } from './lib/router';
 
 export function middleware(request: NextRequest) {
-  const accessToken = request.cookies.get('accessToken')?.value as string;
+  const accessToken = request.cookies.get('token')?.value as string;
 
   const pathname = request.nextUrl.pathname;
 
