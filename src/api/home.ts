65206f376f6ac9ac3a9f1930ad7d0949926108db
5o-hyun <94782559+5o-hyun@users.noreply.github.com>
2025-02-27 14:26:55 +0900

    feat: 붕도감 유저의리스트페이지, 상세페이지, 도감캡쳐

--- src/api/home.ts
+++ src/api/home.ts
@@ -1,6 +1,19 @@
-import { Dogams } from '@/types/home';
+import { DogamDetail, Dogams } from '@/types/home';
 import axios from 'axios';
+import auth from './auth';
 
 export const getDogams = async (): Promise<Dogams> => {
   return await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/dogam/list`);
+};
+
+export const getUserDogams = async (): Promise<Dogams> => {
+  return await auth.get(`${process.env.NEXT_PUBLIC_API_URL}/dogam/userList`);
+};
+
+export const getUserDogamDetail = async (
+  bungId: number,
+): Promise<DogamDetail> => {
+  return await auth.get(`${process.env.NEXT_PUBLIC_API_URL}/dogam`, {
+    params: { bungId },
+  });
 };
